b1802e840d53df38ece9248ec846c33e783dcea9
selddom <selddom@selddoms-MacBook-Pro.local>
2015-07-26 20:15:41 -0700

    a function

--- hello.go
+++ hello.go
@@ -2,7 +2,12 @@ package main
 
 import "fmt"
 
-func main() {
+func dummy() (n int, err string) {
+   return 1, "Error message"
+}
 
+func main() {
 	fmt.Println("Hello, This is GO here!")
+        n, err := dummy()
+        fmt.Printf("n: %v, err: %v", n, err)
 }
