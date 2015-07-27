5d17a754607b7dc49993ef7382a2bf2e355387d1
Selvi Kadirvel <selvikadirvel@gmail.com>
2015-07-26 20:51:01 -0700

    A variadic function

--- hello.go
+++ hello.go
@@ -6,8 +6,20 @@ func dummy() (n int, err string) {
    return 1, "Error message"
 }
 
+func print(numbers ...int) {
+
+   for _, value  := range(numbers) {
+      fmt.Println(value)
+   }
+}
+
 func main() {
 	fmt.Println("Hello, This is GO here!")
         n, err := dummy()
         fmt.Printf("n: %v, err: %v", n, err)
+
+        n1 := 1
+        n2 := 2
+        n3 := 3
+        print(n1, n2, n3) 
 }
